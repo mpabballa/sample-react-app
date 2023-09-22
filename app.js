935cc7209ce6b2cfd56f1460c2946bf4043b0104
Mahesh Pabballa <63855247+mpabballa@users.noreply.github.com>
2023-09-22 16:23:08 +0530

    added package.json and parcel packages

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // const header = document.createElement("h1");
 // header.innerHTML = "Hello Mahesh"
 // const root = document.getElementById("root");
@@ -7,9 +10,9 @@
 const reactHeader = React.createElement('h1',{id: 'headerId', class:'headerClass'}, 'Hello React..!');
 const reactdom = React.createElement('div',{id: 'divid', class:'headerClass'}, 
 [ 'Main header content', React.createElement('h2',{id: 'h2Id', class:'childs'}, 
-[React.createElement('h3',{id: 'h3Id', class:'childs'}, 'Child h3 -1 content exaample'),
-React.createElement('h3',{id: 'h3Id', class:'childs'}, 'Child h3 -2 content exaample'),
-React.createElement('h3',{id: 'h3Id', class:'childs'}, 'Child h3 -3 content exaample'),]),
+[React.createElement('h3',{id: 'h3Id', class:'childs'}, 'Child h3 - 1 content exaample'),
+React.createElement('h3',{id: 'h3Id', class:'childs'}, 'Child h3 - 2 content exaample'),
+React.createElement('h3',{id: 'h3Id', class:'childs'}, 'Child h3 - 3 content exaample'),]),
 React.createElement('h3',{id: 'h3Id', class:'childs'}, 'Child h3 content exaample'), 
 'Test data', ' test data here.']);
 
